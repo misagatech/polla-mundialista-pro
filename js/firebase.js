2ccb03e1fecdd12c5525f2e02cdb18ec44918e3a
Misagatech <misagatech@gmail.com>
2026-05-19 14:40:09 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_MLFy3prhsg3N2T-FTObrFoaQS_K8yuI",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
