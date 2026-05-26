03c996927fd0803086545e4abb5488f0b847d082
Misagatech <misagatech@gmail.com>
2026-05-26 17:55:26 -0500

    vamos de nuevo

--- js/app.js
+++ js/app.js
@@ -53,6 +53,44 @@ let participantsUnsubscribe = null;
 let adminParticipantsUnsubscribe = null;
 let gruposData = {};
 let clasificadosGlobales = {};
+let bracket = {
+  73: {},
+  74: {},
+  75: {},
+  76: {},
+  77: {},
+  78: {},
+  79: {},
+  80: {},
+  81: {},
+  82: {},
+  83: {},
+  84: {},
+  85: {},
+  86: {},
+  87: {},
+  88: {},
+
+  89: {},
+  90: {},
+  91: {},
+  92: {},
+  93: {},
+  94: {},
+  95: {},
+  96: {},
+
+  97: {},
+  98: {},
+  99: {},
+  100: {},
+
+  101: {},
+  102: {},
+
+  103: {},
+  104: {}
+};
 let grupoActivo = "A";
 let adminGrupoActivo = "A";
 
